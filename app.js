3508a1593c904e3feb00d06fe75953899e2b7dd2
julian molina <molina.bohemio@gmail.com>
2020-04-09 15:25:07 -0300

    clean code

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const swaggerUi = require('swagger-ui-express');
 
 const routes = require('./app/routes');
 const errors = require('./app/middlewares/errors');
-const documentation = require('./documentation');
 
 const app = express();
 
 // Client must send "Content-Type: application/json" header
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 routes.init(app);
 
