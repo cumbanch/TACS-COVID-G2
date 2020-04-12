bdabce8fbcb897a5ccfa062750538f5737acac2b
ivodan <ivan.gomez@trocafone.com>
2020-04-12 00:19:23 -0300

    Improving test

--- backend/src/tests/routes.spec.js
+++ backend/src/tests/routes.spec.js
@@ -3,15 +3,17 @@ const app = require('../app');
 
 describe('Health check endpoint', () => {
   let res = {};
-  beforeAll(async () => {
-    res = await request(app)
-      .get('/health')
-      .send();
-  });
-  it('Should return status code 200', () => {
-    expect(res.statusCode).toEqual(200);
-  });
-  it('Should return the uptime', () => {
-    expect(res.body).toHaveProperty('uptime');
+  describe('Successful response', () => {
+    beforeAll(async () => {
+      res = await request(app)
+        .get('/health')
+        .send();
+    });
+    it('Should return status code 200', () => {
+      expect(res.statusCode).toEqual(200);
+    });
+    it('Should return the uptime', () => {
+      expect(res.body).toHaveProperty('uptime');
+    });
   });
 });
