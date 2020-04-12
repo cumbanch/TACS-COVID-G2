0ea2b4462c594b1ca65815b49a1a65dafcd8bee7
ivodan <ivan.gomez@trocafone.com>
2020-04-11 21:14:54 -0300

    Improving endpoint test

---
+++ src/tests/routes.spec.js
@@ -0,0 +1,17 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('Health check endpoint', () => {
+  let res = {};
+  beforeAll(async () => {
+    res = await request(app)
+      .get('/health')
+      .send();
+  });
+  it('Should return status code 200', () => {
+    expect(res.statusCode).toEqual(200);
+  });
+  it('Should return the uptime', () => {
+    expect(res.body).toHaveProperty('uptime');
+  });
+});
