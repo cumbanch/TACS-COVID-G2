2a2ec55a583755ecc9bcc1684ba2f6ec8a0feca5
julian molina <molina.bohemio@gmail.com>
2020-04-12 21:10:51 -0300

    fix minor changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const swaggerUi = require('swagger-ui-express');
-const { inspect } = require('util');
 
 const routes = require('./app/routes');
 const errors = require('./app/middlewares/errors');
 const swaggerDocument = require('./documentation/swagger.json');
-const logger = require('./app/logger');
+const { logRequests } = require('./app/middlewares/logger_requests');
 
 const app = express();
 
@@ -15,14 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.use((req, res, next) => {
-  logger.info(
-    `New request in ${req.path} with method ${req.method} with params: ${inspect(
-      req.params
-    )}, body: ${inspect(req.body)} and query: ${inspect(req.query)}`
-  );
-  next();
-});
+app.use(logRequests);
 routes.init(app);
 
 app.use(errors.handle);
--- backend/src/app/serializers/countries.js
+++
@@ -1,1 +0,0 @@
-exports.countriesListMapper = countries => {};
