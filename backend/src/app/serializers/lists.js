7c6db3cc07a631e0770b152f44a42bf1615c7dea
ivodan <ivan.gomez@trocafone.com>
2020-04-16 19:52:49 -0300

    Remove serializer

--- backend/src/app/serializers/lists.js
+++ backend/src/app/serializers/lists.js
@@ -1,7 +1,3 @@
 const { objectToSnakeCase } = require('../utils/objects');
-const { getCountrySerializer } = require('./countries');
 
-exports.getListSerializer = list => {
-  list.countries = getCountrySerializer(list.countries.map(country => country.dataValues));
-  return objectToSnakeCase(list.dataValues);
-};
+exports.getListSerializer = list => objectToSnakeCase(list.dataValues);
