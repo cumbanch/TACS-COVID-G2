e174684b4ad6423be10bfa0bd91d4e86cb7e1741
ivodan <ivan.gomez@trocafone.com>
2020-04-16 19:55:02 -0300

    Removing unnecessary code

--- backend/src/app/serializers/lists.js
+++ backend/src/app/serializers/lists.js
@@ -1,3 +1,3 @@
 const { objectToSnakeCase } = require('../utils/objects');
 
-exports.getListSerializer = list => objectToSnakeCase(list.dataValues);
+exports.getListSerializer = list => objectToSnakeCase(list);
