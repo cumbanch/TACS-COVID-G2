b233324f4dd4f877969102e5d776eab3f7d8f7b3
ivodan <ivan.gomez@trocafone.com>
2020-04-16 19:31:18 -0300

    Lint fix

--- backend/src/app/serializers/lists.js
+++ backend/src/app/serializers/lists.js
@@ -2,6 +2,6 @@ const { objectToSnakeCase } = require('../utils/objects');
 const { getCountrySerializer } = require('./countries');
 
 exports.getListSerializer = list => {
-    list.countries = getCountrySerializer(list.countries.map(country => country.dataValues));
-    return objectToSnakeCase(list.dataValues);
+  list.countries = getCountrySerializer(list.countries.map(country => country.dataValues));
+  return objectToSnakeCase(list.dataValues);
 };
