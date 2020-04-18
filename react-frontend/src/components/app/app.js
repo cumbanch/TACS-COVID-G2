1dd45484314d30451d2f989770d56859dfb008c7
ivodan <ivan.gomez@trocafone.com>
2020-04-18 19:11:39 -0300

    Translating gatito -> kitten

--- react-frontend/src/components/app/app.js
+++ react-frontend/src/components/app/app.js
@@ -1,12 +1,12 @@
 import React, { Component, useState } from "react";
-import Gatito from '../gatito'
+import Kitten from '../kitten'
 
 const App = (props) => {
 
   // const [todos, setTodos] = useState({ img: props.img, name: props.name });
 
   return (
-    <Gatito img="https://image.shutterstock.com/image-photo/cut-kitten-cup-home-cat-260nw-1494647540.jpg" nombre="Michino" />
+    <Kitten img="https://image.shutterstock.com/image-photo/cut-kitten-cup-home-cat-260nw-1494647540.jpg" nombre="Michino" />
   );
 }
 export default App;
