8a778efa1936307c8f9e6d0a4dea88b6321f264e
loopmeagain <dario.fabricio.sobrino@gmail.com>
2020-04-18 17:58:14 -0300

    cambio a hooks  y estructuras

--- react-frontend/src/components/app/index.js
+++ react-frontend/src/components/app/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from './node_modules/react';
+import ReactDOM from './node_modules/react-dom';
 import './index.css';
-import App from './components/app/app';
+import App from './app';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
