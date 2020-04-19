49864ec2b995420ed6eefa6450f2f0f40089c9aa
ivodan <ivan.gomez@trocafone.com>
2020-04-18 23:29:34 -0300

    Updating components (login and sign up) to work with hooks

--- react-frontend/src/components/login/login.js
+++ react-frontend/src/components/login/login.js
@@ -1,30 +1,31 @@
-import React, { Component } from 'react';
+import React from "react";
 
-export default class Login extends Component {
-    render() {
-        return (
-            <form>
-                <h3>Sign In</h3>
+const Login = () => {
+    return (
+        <form>
+            <h3>Sign In</h3>
 
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+            <div className="form-group">
+                <label>Email address</label>
+                <input type="email" className="form-control" placeholder="Enter email" />
+            </div>
+
+            <div className="form-group">
+                <label>Password</label>
+                <input type="password" className="form-control" placeholder="Enter password" />
+            </div>
+
+            <div className="form-group">
+                <div className="custom-control custom-checkbox">
+                    <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                    <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
                 </div>
+            </div>
 
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
-
-                <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                    </div>
-                </div>
-
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
-            </form>
-        );
-    }
+            <button type="submit" className="btn btn-primary btn-block">Submit</button>
+        </form>
+    );
 }
+
+
+export default Login;
