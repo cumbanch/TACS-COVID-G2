df5369a9b69e0101939632c512f6866f6b4b179c
julian molina <molina.bohemio@gmail.com>
2020-04-19 13:12:37 -0300

    fix list service

--- backend/src/app/services/lists.js
+++ backend/src/app/services/lists.js
@@ -80,7 +80,7 @@ exports.getAllList = filters => {
   logger.info(`Attempting to get lists with filters: ${inspect(filters)}`);
   return List.findAndCountAll({
     where: {
-      userId: filters.user.id
+      userId: filters.userId
     },
     offset: (filters.page - 1) * filters.limit,
     limit: filters.limit,
@@ -96,7 +96,7 @@ exports.getList = (filters, options = {}) => {
   return List.findOne({
     where: {
       id: filters.id,
-      userId: filters.user.id
+      userId: filters.userId
     },
     ...options
   }).catch(err => {
