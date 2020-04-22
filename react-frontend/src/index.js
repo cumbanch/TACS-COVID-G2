758d5c3d29f31ef7812a4ea1aac47e35e4fd2cc6
ivodan <ivan.gomez@trocafone.com>
2020-04-21 22:43:39 -0300

    Using new AppComponent name in index.js

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './assets/sass/global.scss';
-import App from './components/app/app';
+import AppComponent from './components/app/app';
 import * as serviceWorker from './service-worker';
 
 ReactDOM.render(
     <BrowserRouter>
-        <App />
+        <AppComponent />
     </BrowserRouter>,
     document.getElementById('root')
 );
