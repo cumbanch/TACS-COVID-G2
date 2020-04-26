60b27ab31f04281b3cbe7726b7843835a7d0e5b8
ivodan <ivan.gomez@trocafone.com>
2020-04-26 20:54:45 -0300

    Removing hashing from frontend

--- react-frontend/src/services/sessions.js
+++ react-frontend/src/services/sessions.js
@@ -1,45 +1,32 @@
-import bcrypt from 'bcryptjs';
-
 const axios = require('axios').default;
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080/',
-    timeout: 1000,
-    headers: {'Content-Type': 'application/json'}
+  baseURL: 'http://localhost:8080/',
+  timeout: 1000,
+  headers: {'Content-Type': 'application/json'}
 });
 
 const salt = 10;
 
-const hashPassword = (password) => {
-    return bcrypt.hash(password, salt);
-}
-
 
 export const SignUp = (attributes) => {
-    return hashPassword(attributes.password1)
-        .then(hash => {
-            return axiosInstance.post('/users',
-                JSON.stringify(
-                    {
-                        'name': attributes.firstName,
-                        'last_name': attributes.lastName,
-                        'email': attributes.email,
-                        'password': hash
-                    }
-                ));
-        });
+  return axiosInstance.post('/users',
+    JSON.stringify(
+      {
+        'name': attributes.firstName,
+        'last_name': attributes.lastName,
+        'email': attributes.email,
+        'password': attributes.password1
+      }
+    ));
 }
 
 export const SignIn = (attributes) => {
-    return hashPassword(attributes.password)
-        .then(hash => {
-            console.log(hash);
-            return axiosInstance.post('/sessions/login',
-                JSON.stringify(
-                    {
-                        'email': attributes.email,
-                        'password': hash
-                    }
-                ));
-        });
+  return axiosInstance.post('/sessions/login',
+    JSON.stringify(
+      {
+        'email': attributes.email,
+        'password': attributes.password
+      }
+    ));
 }
