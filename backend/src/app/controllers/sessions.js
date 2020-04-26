4d974ba4f02dcd16166135b59623bedea6690cf5
ivodan <ivan.gomez@trocafone.com>
2020-04-26 20:12:06 -0300

    Hashing password when creating user and comparing when login it

--- backend/src/app/controllers/sessions.js
+++ backend/src/app/controllers/sessions.js
@@ -1,16 +1,21 @@
 const { getUserBy } = require('../services/users');
-const { notFound } = require('../errors/builders');
-const { generateTokens, verifyAndCreateToken } = require('../services/sessions');
+const { notFound, invalidCredentials } = require('../errors/builders');
+const { generateTokens, verifyAndCreateToken, comparePassword } = require('../services/sessions');
 const { login, refresh } = require('../serializers/sessions');
 const { createTokenBlacklist } = require('../services/tokens_black_list');
 
 exports.login = (req, res, next) =>
-  getUserBy({ email: req.body.email, password: req.body.password })
+  getUserBy({ email: req.body.email })
     .then(user => {
       if (!user) throw notFound('User not found');
-      return generateTokens({ user, req }).then(([accessToken, idToken, refreshToken]) =>
-        res.status(200).send(login({ accessToken, idToken, refreshToken }))
-      );
+      return comparePassword(req.body.password, user.password)
+        .then(match => {
+          if (!match) throw invalidCredentials();
+          return generateTokens({ user, req }).then(([accessToken, idToken, refreshToken]) =>
+            res.status(200).send(login({ accessToken, idToken, refreshToken }))
+          );
+        })
+        .catch(next);
     })
     .catch(next);
 
