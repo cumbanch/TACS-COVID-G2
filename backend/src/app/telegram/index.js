34e09d0522998700e02350aeeddf495f30a0a42c
tatiana <tmanoja@gmail.com>
2020-04-27 12:55:26 -0300

    -fix: help const

--- backend/src/app/telegram/index.js
+++ backend/src/app/telegram/index.js
@@ -4,8 +4,7 @@ const { apiKey } = require('../../config').telegram;
 const { getTelegramLogin } = require('../telegram/sessions');
 
 
-exports.help = () => {
-    return `Welcome to the COVID-19 Bot!
+const help = `Welcome to the COVID-19 Bot!
 This bot was created by the Group 2 of TACS
 UTN-FRBA
 
@@ -24,14 +23,13 @@ You can control me by sending these commands:
 /countries - get the list of countries in a list
 
 Please, start to login`;
-}
 
 exports.telegram = () => {
     const bot = new TeleBot({
         token: apiKey,
         usePlugins: ['commandButton']
     });
-    bot.on(['/start', '/help'], (msg) => msg.reply.text(this.help()));
+    bot.on(['/start', '/help'], (msg) => msg.reply.text(help));
     bot.on(/^\/login (.+) (.+)$/, (msg, props) => getTelegramLogin(props.match[1], props.match[2], msg.from.id)
         .then((response) => msg.reply.text(response))
     );
