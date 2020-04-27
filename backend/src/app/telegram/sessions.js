b367e1feb15b2d888eec1a6b50d1ca1789c52351
tatiana <tmanoja@gmail.com>
2020-04-27 18:58:59 -0300

    -feat: telegram password hashed

--- backend/src/app/telegram/sessions.js
+++ backend/src/app/telegram/sessions.js
@@ -1,10 +1,14 @@
 const { getUserBy } = require('../services/users');
 const { createTelegram } = require('../services/telegram');
+const { comparePassword } = require('../services/sessions');
 
 exports.getTelegramLogin = (email, password, chatId) =>
-    getUserBy({ email: email, password: password })
+    getUserBy({ email: email })
         .then(user => {
             if (!user) return 'User not found';
-            return createTelegram({ chatId: `${chatId}`, userId: user.id })
-                .then(() => `Welcome ${user.name} ${user.lastName}!`);
+            return comparePassword(password, user.password).then(match => {
+                if (!match) throw invalidCredentials();
+                return createTelegram({ chatId: `${chatId}`, userId: user.id })
+                    .then(() => `Welcome ${user.name} ${user.lastName}!`);
+            });
         });
