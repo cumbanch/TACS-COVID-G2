a47270e6c5a07254f7281cd0064dabb7fc40f23b
nicopatronelli <nicopatronelli@gmail.com>
2020-05-26 17:28:16 -0300

    Admin sidemenu & refactors

--- react-frontend/src/components/dashboards/countries-dashboard.js
+++ react-frontend/src/components/dashboards/countries-dashboard.js
@@ -1,7 +1,51 @@
 import React, { useState } from 'react'
-const UsersComponent = (props) => {
-    return (
-        <div />
-    );
+import axios from 'axios';
+
+const getAccessToken = () => {
+    const tokens = JSON.parse(localStorage.getItem('userInfo'));
+    return tokens.access_token;
 }
-export default UsersComponent;
+
+
+const CountriesComponent = (props) => {
+
+    // const getCountriesList = async () => {
+    //     const response = await axios.get(
+    //         "http://localhost:8080/countries/",
+    //         {
+    //             headers: {
+    //                 'Accept': 'application/json',
+    //                 'Content-Type': 'application/json',
+    //                 'Access-Control-Allow-Origin': 'https://localhost:8080',
+    //                 'Authorization': getAccessToken(),
+    //             }
+    //         });
+    //     return response.data;
+    // };
+
+    // const listOfCountries = getCountriesList();
+    // console.log(listOfCountries);
+
+    // return (
+    //     <div>
+    //         <h1>Resultado</h1>
+    //         <h1>Resultado</h1>
+    //         <h1>Resultado</h1>
+    //         <h1>Resultado</h1>
+    //         <h1>Resultado</h1>
+    //         <h1>Resultado</h1>
+    //         <h1>{listOfCountries.length}</h1>
+    //         {/* {listOfCountries.map(c => {
+    //             return (<h6>{c.name}</h6>)
+    //         }
+    //         )} */}
+    //     </div>
+    // );
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default CountriesComponent;
