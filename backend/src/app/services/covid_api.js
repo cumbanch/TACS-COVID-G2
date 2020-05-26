b60234375dd69896832d97adc9d9c008071d06f5
Iván Gómez Yaury <ivan.gomez@trocafone.com>
2020-05-25 23:48:17 -0300

    offsets

--- backend/src/app/services/covid_api.js
+++ backend/src/app/services/covid_api.js
@@ -4,6 +4,7 @@ const { inspect } = require('util');
 const logger = require('../logger');
 const { baseUrl, timeseriesEndpoint, latestEndpoint } = require('../../config').covidApi;
 const { externalService, notFound } = require('../errors/builders');
+const { moment } = require('../utils/moment');
 
 exports.getLatestByIso2 = iso2 => {
   const url = `${baseUrl}${latestEndpoint}${iso2}`;
@@ -20,6 +21,56 @@ const getTimeseriesByIso2 = (iso2, transformResponse) => {
 
 const getTimeseries = (country, transformResponse) =>
   getTimeseriesByIso2(country.dataValues.iso2, transformResponse);
+
+const getBlankTimeserie = date => ({
+  confirmed: 0,
+  deaths: 0,
+  recovered: 0,
+  date
+});
+
+const processNegativeOffset = country => {
+  const firstDate = moment(country.timeseries[0].date, 'YYYY/MM/DD');
+  const offset = Math.abs(country.offset);
+  for (let i = 0; i < offset; i++) {
+    firstDate.subtract(1, 'days');
+    country.timeseries.unshift(getBlankTimeserie(firstDate.format('YYYY/MM/DD')));
+  }
+  country.offset = 0;
+};
+
+const processPositiveOffset = country => {
+  let i = 0;
+  for (; i < country.offset; i++) {
+    if (country.timeseries[0].confirmed === 0) country.timeseries.shift();
+    else break;
+  }
+  country.offset -= i;
+};
+
+const someCountryHasOffsetToProcess = countries => countries.some(country => country.data.offset !== 0);
+
+const getCountryWithOffsetToProcess = countries => countries.filter(country => country.data.offset)[0];
+
+const processOffsets = countries => {
+  let country = null;
+  let otherCountries = null;
+  while (someCountryHasOffsetToProcess(countries)) {
+    country = getCountryWithOffsetToProcess(countries);
+    if (country.data.offset < 0) {
+      processNegativeOffset(country.data);
+    } else if (country.data.offset > 0) {
+      processPositiveOffset(country.data);
+      if (country.data.offset > 0) {
+        // eslint-disable-next-line no-loop-func
+        otherCountries = countries.filter(otherCountry => otherCountry.data.id !== country.data.id);
+        // eslint-disable-next-line no-loop-func
+        otherCountries.forEach(otherCountry => (otherCountry.data.offset -= country.data.offset));
+        country.data.offset = 0;
+      }
+    }
+  }
+};
 
 exports.getLatestByList = list => {
   if (!list) {
@@ -45,7 +96,7 @@ exports.getLatestByList = list => {
     });
 };
 
-exports.getTimeseriesByList = list => {
+exports.getTimeseriesByList = (list, offsets) => {
   const promises = list.countries.map(country =>
     getTimeseries(country, data => {
       const parsedData = JSON.parse(data)[0];
@@ -54,11 +105,45 @@ exports.getTimeseriesByList = list => {
         countryFounded = list.countries.find(({ iso2 }) => iso2 === parsedData.countrycode.iso2);
         delete countryFounded.dataValues.CountryByList;
       }
-      return parsedData ? { ...countryFounded.dataValues, timeseries: parsedData.timeseries } : undefined;
+      if (!parsedData) return undefined;
+      const timeseries = Object.keys(parsedData.timeseries).map(date => ({
+        ...parsedData.timeseries[date],
+        date: moment(date, 'M/DD/YY').format('YYYY/MM/DD')
+      }));
+      return { ...countryFounded.dataValues, timeseries };
     })
   );
-  return axios.all(promises).catch(err => {
-    logger.error(inspect(err));
-    throw externalService(`There was an error getting the history, reason: ${err.message}`);
-  });
+  return axios
+    .all(promises)
+    .then(countries => {
+      const firstDateOfList = countries
+        .map(country => {
+          let firstDateOfCountry = null;
+          country.data.timeseries.some(timeserie => {
+            if (timeserie.confirmed !== 0) {
+              firstDateOfCountry = timeserie.date;
+              return true;
+            }
+            return false;
+          });
+          return firstDateOfCountry;
+        })
+        .sort()[0];
+      countries.forEach(country => {
+        country.data.timeseries = country.data.timeseries.filter(
+          timeserie => timeserie.date >= firstDateOfList
+        );
+        const offsetsForCountry = offsets.filter(
+          offsetCountry => offsetCountry.country_id === country.data.id
+        );
+        const offsetForCountry = offsetsForCountry[0];
+        country.data.offset = offsetForCountry ? offsetForCountry.offset : 0;
+      });
+      processOffsets(countries);
+      return countries;
+    })
+    .catch(err => {
+      logger.error(inspect(err));
+      throw externalService(`There was an error getting the history, reason: ${err.message}`);
+    });
 };
